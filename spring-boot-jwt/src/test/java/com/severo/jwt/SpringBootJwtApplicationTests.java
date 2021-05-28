94413792277e0b023c82e66f2be6dd2def41073f
robinsonvs <robinsonvs@gmail.com>
2021-05-27 21:34:47 -0300

    first commit

---
+++ spring-boot-jwt/src/test/java/com/severo/jwt/SpringBootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.severo.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
