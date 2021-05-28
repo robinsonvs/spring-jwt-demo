fdb87cad917e1230180a561c85ae3efdfa8fa49e
robinsonvs <robinsonvs@gmail.com>
2021-05-27 21:46:56 -0300

    created userData

---
+++ spring-boot-jwt/src/main/java/com/severo/jwt/data/UserData.java
@@ -0,0 +1,18 @@
+package com.severo.jwt.data;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserData implements Serializable {
+
+    private String userName;
+
+    private String password;
+
+}
